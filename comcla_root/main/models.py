e20599837ffe093028152d2f3f6d6cf1967f0b1e
ADIKOLORD <adiko23082004@gmail.com>
2022-04-27 11:12:31 +0600

    feature/add_index/add_setting_static/add_model_contact/

--- comcla_root/main/models.py
+++ comcla_root/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    email = models.CharField(max_length=100, unique=True)
+    phone = models.CharField(max_length=100, verbose_name='Номер телефона')
+    message = models.TextField(verbose_name='Текст')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Контакты'
--- comcla_root/main/views.py
+++ comcla_root/main/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from main.models import Contact
+
+
+def main_page(request):
+    if request.method == 'POST':
+        name = request.POST['Name']
+        email = request.POST['Email']
+        phone = request.POST['Phone Number']
+        text = request.POST['Message']
+        if name != '' and email != '' and text != '':
+            Contact.objects.create(name=name, email=email, phone=phone, message=text)
+        return redirect('home')
+
+    return render(request, 'index.html')
